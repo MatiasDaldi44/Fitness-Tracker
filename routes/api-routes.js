b368403ad2f6c0795df2d7ca80db4b875e2d4cdf
MD24 <matiasdaldi@gmail.com>
2020-08-13 16:38:37 -0400

    added api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,43 @@
+const Workout = require("../models/workout");
+
+module.exports = function (app) {
+    app.get("/api/workouts", (req, res) => {
+        Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    });
+
+    app.post("/api/workouts", ({ body }, res) => {
+        Workout.create(body)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        Workout.update({ _id: req.params.id }, { $push: { exercises: req.body } })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+
+    app.get("/api/workouts/range", ({ body }, res) => {
+        Workout.find({})
+            .then(range => {
+                res.json(range);
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    });
+}
